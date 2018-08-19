13401dba9fa156060a5be784139e172724d630d8
Sushruth90 <sushruth.p18090@iimtrichy.ac.in>
2018-08-19 19:51:23 +0530

    Dude

--- file1.R
+++ file1.R
@@ -1,1 +1,13 @@
 mtcars
+git config --global user.email "sushruth.p18090@iimtrichy.ac.in"
+git config --global user.name "Sushruth90"
+women
+mean(women$height)
+hist(women$height)
+hist(women$weight)
+plot(women)
+install.packages('car')
+
+?women
+x=1:100
+mean(x)
---
+++ file2.R
@@ -0,0 +1,2 @@
+y= seq(1,100,by=2)
+plot(women)
